481b874ad61df19f8196ceba60cb520bb536baac
AlexChapin <122164689+AlexChapin@users.noreply.github.com>
2024-04-09 22:27:23 -0400

    Added Line By Line Comments

--- Marble_Sorter.c
+++ Marble_Sorter.c
@@ -7,54 +7,54 @@
 
 
 
-void intake()
+void intake() //Function To Intake One Marble into The Sensor Area.
 {
-	while(SensorValue(InitQuad)<5){
+	while(SensorValue(InitQuad)<5){ //Run the motor up until we reach an encoder position of 5.
 		startMotor(InitMotor, -30);
 	}
-	startMotor(InitMotor, 127);
+	startMotor(InitMotor, 127); //Run the motor back down until the encoder reaches a position of 2. 
 	while (SensorValue(InitQuad)>2){
-		startMotor(InitMotor, 60);
+		startMotor(InitMotor, 60); // Slow down the motor to create a nice deceleration curve to stop the marble.
 	}
-	stopMotor(InitMotor);
+	stopMotor(InitMotor); // Stop the motor after.
 }
 
 
-void checkmarble()
+void checkmarble() // Checks to see if there is a marble
 {
-	if(SensorValue(Line) > 2685)
+	if(SensorValue(Line) > 2685) //Is Marble Transparent?
 	{
 		setServo(DirectionGate, 0);
 		wait(.5);
-		setServo(ColorGate, 30);
+		setServo(ColorGate, 30); //Send To Bin 1
 		wait(3);
 		setServo(ColorGate, -40);
 	}
-	if (SensorValue(Line) < 2350)
+	if (SensorValue(Line) < 2350) //Is Marble Plastic?
 	{
 		setServo(DirectionGate, -50);
 		wait(.5);
-		setServo(ColorGate, 30);
+		setServo(ColorGate, 30); // Send To Bin 2
 		wait(3);
 		setServo(ColorGate, -40);
 	}
-	if(SensorValue(Line) < 2550)
+	if(SensorValue(Line) < 2550) //Is Marble Wood?
 	{
 		setServo(DirectionGate, 100);
 		wait(.5);
-		setServo(ColorGate, 30);
+		setServo(ColorGate, 30); // Send To Bin 3
 		wait(3);
 		setServo(ColorGate, -40);
 	}
-	if(SensorValue(Line) < 2680)
+	if(SensorValue(Line) < 2680) //Is Marble Metal?
 	{
 		setServo(DirectionGate, -120);
 		wait(.5);
-		setServo(ColorGate, 30);
+		setServo(ColorGate, 30); //Send To Bin 4
 		wait(3);
 		setServo(ColorGate, -40);
 	}
-SensorValue(InitQuad) = 0;
+SensorValue(InitQuad) = 0; //Finish Zeroring Encoder For Reliablilty 
 stopMotor(InitMotor);
 }
 
@@ -66,14 +66,14 @@ task main()
 	startMotor(InitMotor, 20);
 	wait(3);
 	SensorValue(InitQuad) = 0;
-	wait(.05);
+	wait(.05); //Initialy Zero Encoder
 
-	while(true){
-		setServo(ColorGate, -40);
-		intake();
-		startMotor(InitMotor, 20);
+	while(true){ //Repeat Forever
+		setServo(ColorGate, -40); //Move Gate To Lower Conditition
+		intake(); //Move A Marble Down The System To The Sorter
+		startMotor(InitMotor, 20); //Move TThe Gate Motor Down To Zero Encoder
 		wait(3);
-		checkmarble();
+		checkmarble(); //Determine what Color Marble Is In The Sorter And Sort It
 
 	}
 }
